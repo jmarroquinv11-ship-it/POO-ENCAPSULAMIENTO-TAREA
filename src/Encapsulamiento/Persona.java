c54fef0f8bc6506441b6e24894463b48277b0076
Juanpii_02 <juanpablvasquez1234@gmail.com>
2026-06-22 06:04:38 -0600

    Ejercicio 1 Persona

---
+++ src/Encapsulamiento/Persona.java
@@ -0,0 +1,20 @@
+package Encapsulamiento;
+
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public void mostrar() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Edad: " + edad);
+    }
+}
